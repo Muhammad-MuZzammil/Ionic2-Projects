848aa155dc13992d708d62c4db4174263d797a5f
Muhammad-MuZzammil <Muhammadmuzzammil10@hotmail.com>
2016-07-11 20:11:56 +0500

    404 Error

--- TodoApp/app/app.ts
+++ TodoApp/app/app.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
+import { FIREBASE_PROVIDERS, defaultFirebase, firebaseAuthConfig, AuthProviders, AuthMethods } from 'angularfire2';
+
 import {HomePage} from './pages/home/home';
 
 
@@ -10,7 +12,15 @@ import {HomePage} from './pages/home/home';
 export class MyApp {
   rootPage: any = HomePage;
 
+  
+
   constructor(platform: Platform) {
+
+
+
+
+   
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +29,15 @@ export class MyApp {
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp,[
+  
+ FIREBASE_PROVIDERS,
+    defaultFirebase({
+        apiKey: "AIzaSyBZRn-Be7LtepznbN4C_ht7Qcry8fTaJnQ",
+        authDomain:  "todoapp-c9351.firebaseapp.com",
+        databaseURL: "https://todoapp-c9351.firebaseio.com",
+        storageBucket: "todoapp-c9351.appspot.com",
+    })
+
+
+  ]);
