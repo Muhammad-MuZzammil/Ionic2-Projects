b2d5ec6b5d0af5ed6e1e37098fb6fc7bae83ee81
Muhammad-MuZzammil <Muhammadmuzzammil10@hotmail.com>
2016-08-16 11:07:25 +0500

    using ngfire2 Retrieving data as lists

--- Practice App/TodoApp with angularfire2/app/pages/home/home.ts
+++ Practice App/TodoApp with angularfire2/app/pages/home/home.ts
@@ -6,21 +6,59 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage implements OnInit {
-
+  show: boolean = false;
+  edit: boolean = true
   items: FirebaseListObservable<any[]>;
   constructor(private af: AngularFire) {
   }
- ngOnInit(){
-   
- }
- addItem(takeitem:HTMLInputElement){
-console.log(takeitem.value);
-   
+  ngOnInit() {
     this.items = this.af.database.list('items')
-    this.items.push(takeitem.value)
-    .then(result => console.log('data pushed successfully', result))
-    .catch(err => console.log(err))
+    // console.log(this.items);
+// this.items=this.af.database.list('items',{preserveSnapshot:true});
+//     this.items.subscribe(snapshots => {
+//       snapshots.forEach(snapshot => {
+//           console.log(snapshot.key)
+//           console.log(snapshot.val)
+//       }); 
+//     })
+    
+    
 
- }
 
+  }
+  showData() {
+    this.show = false
+    this.edit = true;
+
+    this.items = this.af.database.list('items')
+
+  }
+  editItem() {
+    this.show = true;
+    this.edit = false;
+
+  }
+  addItem(pushItem: HTMLInputElement) {
+    this.items.push({ text: pushItem })
+      .then(success => console.log('push data Successfully', success))
+      .catch(err => console.log('404 Error', err));
+  }
+
+  updateBtn(key: string, updateData: HTMLInputElement) {
+    this.show = true;
+
+    this.items.update(key, { text: updateData })
+      .then(success => console.log('data updated', success))
+      .catch(err => console.log('404 Error', err));
+
+  }
+  deleteItem(key: string) {
+    this.items.remove(key)
+      .then(success => console.log('Delete Item Successfully', success))
+      .catch(err => console.log('404 Error', err));
+  }
+  deleteAll() {
+    this.items.remove();
+    
+  }
 }
