8c372cb97ca3625590e180cb4290f6c8b3bb6de6
Muhammad-MuZzammil <Muhammadmuzzammil10@hotmail.com>
2016-09-01 08:06:40 +0500

    initial commit

---
+++ Practice App rc3/githubIonic/app/providers/github-users/github-users.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { User } from '../../model/user';
+
+/*
+  Generated class for the GithubUsers provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class GithubUsers {
+
+  githubUsers: any = null;
+  constructor(private http: Http) { }
+  
+  load() {
+    if (this.githubUsers) {
+      return Promise.resolve(this.githubUsers);
+    }
+    return new Promise(resolve => {
+      this.http.get('http://api.github.com/users')
+      .map(res=><Array<User>>(res.json()))
+      .subscribe(users=>{
+        this.githubUsers=users;
+        resolve(this.githubUsers);
+      })
+    })
+  }
+
+}
+
